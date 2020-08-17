9fa8fe6d6b3dfc1c4ef39b0df29da43f8f79c08d
zanomate <zanomate@gmail.com>
2020-08-17 22:18:01 +0200

    syntax | Size accept special value "fill"

--- modules/syntax/src/model/Size.ts
+++ modules/syntax/src/model/Size.ts
@@ -1,7 +1,16 @@
-export type Size = number | string
+export enum SpecialSize {
+  FILL = 'fill'
+}
 
-export const parseSize = (value?: Size): string| undefined => {
-  if (parseSize === undefined) return undefined
+export type Size = number | string | SpecialSize
+
+export const parseSize = (value?: Size): string => {
   if (typeof value === 'number') return `${value}em`
-  return value
+  if (typeof value === 'string') {
+    if (value === SpecialSize.FILL) {
+      return '100%'
+    }
+    return value
+  }
+  return 'unset'
 }
